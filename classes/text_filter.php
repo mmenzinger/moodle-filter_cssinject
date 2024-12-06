a5c89a896023b2b7a100d3f134ac22e201169b90
Manuel Menzinger <menzinger@pm.me>
2024-12-06 10:02:21 +0100

    added inline-box functionality

--- classes/text_filter.php
+++ classes/text_filter.php
@@ -18,6 +18,8 @@ class text_filter extends \base_text_filter {
         $pattern = '/(\s*<p>)?\s*\[\!(style|class|box|page):([^\]]*)\!\]\s*(<\/p>\s*)?/';
         // to be able to show the pattern, [\!style: ...!] can be used and the \ will be removed
         $demo_pattern = '/\[(\\\\)\!(style|class|box|page):([^\]]*)\!\]/';
+        // Replace [!box-start: ...!] ... [!box-end!] and also surrounding <p> tags
+        $inline_box_pattern = '/(\s*<p>)?\s*\[\!box-start:([^\]]*)\!\]\s*(<\/p>\s*)?(.*?)(\s*<p>)?\s*\[\!box-end\!\]\s*(<\/p>\s*)?/s';
         $class_prefix = 'cssinject_';
         $box_prefix = "${class_prefix}box_";
 
@@ -25,6 +27,7 @@ class text_filter extends \base_text_filter {
         $class = "";
         $box = "";
         $page = "";
+        // extract css for style, class, box and page from text
         if (preg_match_all($pattern, $text, $matches)) {
             for($i = 0; $i < count($matches[0]); $i++) {
                 $type = $matches[2][$i];
@@ -47,21 +50,38 @@ class text_filter extends \base_text_filter {
             }
         }
 
+        // replace inline box
+        $text = preg_replace_callback($inline_box_pattern, function($matches) use ($box_prefix) {
+            $content = $matches[4];
+            $css = preg_replace_callback('/\b(\w+)\b/', function($matches) use ($box_prefix) {
+                return $box_prefix . $matches[0];
+            }, $matches[2]);
+            return "<div class=\"${box_prefix}container $css\">
+                <div class=\"${box_prefix}area_header\"></div>
+                <div class=\"${box_prefix}area_content\">${content}</div>
+            </div>";
+        }, $text);
+
+        // remove cssinject tags from text
         $text = preg_replace($pattern, '', $text);
+        // correct demo paddern (remove \)
         $text = preg_replace($demo_pattern, '[!$2:$3!]', $text);
 
+        // apply container with header and content if it is a box
         if($box){
             $text = "<div style=\"$style\" class=\"${box_prefix}container $box $class\">
                 <div class=\"${box_prefix}area_header\"></div>
                 <div class=\"${box_prefix}area_content\">$text</div>
             </div>";
         }
+        // else simply apply style and class to surrounding div
         else if($style || $class){
             $text = "<div style=\"$style\" class=\"$class\">$text</div>";
         }
 
+        // apply page css at the end of the text
         if($page){
-            $text = "<style>$page</style>".$text;
+            $text = "$text<style>$page</style>";
         }
 
         return $text;
